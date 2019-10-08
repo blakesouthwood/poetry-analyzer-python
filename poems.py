98313279007ff85c666603b377456b5edad5c84f
blakesouthwood <47462525+blakesouthwood@users.noreply.github.com>
2019-10-08 08:50:02 -0700

    Add files via upload

---
+++ poems.py
@@ -0,0 +1,207 @@
+#module poems  with poems.py
+
+
+
+homegardenerpoem = '''
+Gray white-edged clouds
+blow through the sky,
+seemingly swept along
+by dancing switches
+of white blossoms:
+quaint hearth brooms made from
+our ancient plum, its pale green lichen
+crusting its soft-gray limbs.
+I hold on to the tree,
+cautious from tripping
+on gopher excavations.
+I watch you down the years
+tending the yard:  mowing,
+weeding, raking, pruning.
+Now It smells of new blooms:
+roses, nasturtiums,
+honey locust, Scotch broom.
+Bright-blue stellar jays caw
+in the oak. A neighbor’s
+black and white cat startles at their squawks,
+then rolls around in your freshly-weeded
+rose-garden dirt as if it were catnip.
+You stretch as you stand up,
+and smile, satisfied with
+the piles of weeds destined
+for the compost, “found”
+earthworms to circulate air too.
+'''
+
+
+theviewpoem = '''
+Gray white-edged clouds
+blow by,
+switches
+of white blossoms seeming to sweep them
+along like quaint hearth brooms made from
+ancient plum -- pale green lichen
+crusting soft-gray limbs.
+I hold on to you,
+cautious from tripping.
+Was the view always this clear?
+Houses like white gift boxes
+ribboned with waving greens,
+rimmed on the far side by
+slate-gray bay, camel hills and sky.
+We stand on the edge
+of the cliff every day.
+I hang onto the plum’s rough trunk,
+stare at the view...memorizing it with you.
+'''
+
+
+
+
+catpoem = '''
+CAT AND I
+
+My cat and I
+sat on the stairs
+leading to the front porch
+last night
+in a golden arc of light.
+A cricket chirped in the thicket
+of English ivy and light blue-green juniper
+tangled around the bright white railing.
+
+My entranced, long-haired cat,
+purred as he sat on a steep step.
+With the rhythmic throbs
+of the cricket’s sharp chirps
+and my cat’s soothing purrs,
+I was content.
+'''
+
+
+
+december = '''
+
+I heard the bell
+announce Noel -
+a midnight bell -
+Emanuel.
+
+Like buoy bells,
+marking channels,
+stand sentinel
+in salty swells,
+
+my fears dispel
+with their tinkles.
+
+'''
+
+
+
+
+
+treebites = '''
+
+A eucalyptus branch scared the
+bejesus out of me on a sweaty summer day,
+when it landed next to where I stood with a rush of
+wind and its leaves rustling on dark soft earth.
+
+This old thick tree has wind-peeled bark --
+stiff curling strips that crunch underfoot.
+Its lofty top sways fragrant
+twilight-tinted leaves.
+It hosts myriad hummingbirds,
+an iridescent swirl of dazzling blues and greens.
+They whiz around, up and down,
+tasting tangerine geraniums and worrying my eyes
+with their needle-sharp beaks and whirring thrums.
+
+My son, topping the tree
+among sighing leaves,
+twice heard a living creature utter
+“Unh-UNH-unh” after he drew a saw
+through branches soft as butter.
+He scrambled to the ground, a curious
+look on his face.
+
+His legs started itching
+in three days.
+The tree’s teeth
+turned out to be at its base.
+The tree’s teeth have a name.
+It is POISON OAK.
+My son scratched as he shouted,
+“I’ll be ready for you next time!”
+
+'''
+
+
+
+
+
+
+
+
+oldpath = '''
+
+Like shade from white lace parasols,
+below the overhanging trees
+sunlight and shadow dance a reel.
+Wind sways the leaf-green canopy.
+
+The path we jogged on long ago
+is still the one we stroll along,
+while those new neighbors we don’t know
+lope right on past us, being young.
+
+When I’m at home and I recall
+the fragrant bay and twisting oak,
+I feel the aura mystical
+that’s present in the woods we walk.
+
+'''
+
+
+
+holmesappears = '''
+
+Spring 1997
+
+A Seance?
+
+A dozen of us, readers,
+are gathered in a circle
+of upholstered chairs
+on an autumn night
+while outside the wind rustles
+eucalyptus leaves.
+Lamps cast circles of light
+in the redwood-paneled room.
+We’re aficionados
+of Sherlock Holmes,
+his habits, his intelligence.
+We discuss Dr. Bell,
+role model for Holmes’s methods
+and attention to detail.
+
+I’m suddenly aware that
+In the center of the circle,
+by a redwood-burl coffee table,
+there’s a powerful other presence:
+Holmes is among us,
+no doubt standing tall,
+in tweeds and deerstalker,
+brought to life by us all.
+He exudes the aura
+of nights on the moor,
+and a howling hound,
+of midnight knocks at 221B,
+of cement-gray drifting fog
+obscuring cobblestoned Baker Street,
+of challenges from Moriarty,
+and the impish Irene,
+footprints in mud,
+messages scrawled in blood.
+And edifying chats with dear Dr. Watson.
+'''
--- poetry_english_analyzer.py
+++ poetry_english_analyzer.py
@@ -23,117 +23,45 @@
 The two functions are beneath the poems.
 
  count_word_match()   counts the number of times a word is used in one large string
- 
+
  search_for_phrase()  searches through dictionary for a phrase in lots of large strings
 
 '''
 
+# module with all of the poems stored as strings
+import poems
 
 
-homegardenerpoem = '''
-Gray white-edged clouds
-blow through the sky,
-seemingly swept along
-by dancing switches
-of white blossoms:
-quaint hearth brooms made from
-our ancient plum, its pale green lichen
-crusting its soft-gray limbs.
-I hold on to the tree,
-cautious from tripping
-on gopher excavations.
-I watch you down the years
-tending the yard:  mowing,
-weeding, raking, pruning.
-Now It smells of new blooms:
-roses, nasturtiums,
-honey locust, Scotch broom.
-Bright-blue stellar jays caw
-in the oak. A neighbor’s
-black and white cat startles at their squawks,
-then rolls around in your freshly-weeded
-rose-garden dirt as if it were catnip.
-You stretch as you stand up,
-and smile, satisfied with
-the piles of weeds destined
-for the compost, “found”
-earthworms to circulate air too.
-'''
+# ==== dictionary ====
 
-
-theviewpoem = '''
-Gray white-edged clouds
-blow by,
-switches
-of white blossoms seeming to sweep them
-along like quaint hearth brooms made from
-ancient plum -- pale green lichen
-crusting soft-gray limbs.
-I hold on to you,
-cautious from tripping.
-Was the view always this clear?
-Houses like white gift boxes
-ribboned with waving greens,
-rimmed on the far side by
-slate-gray bay, camel hills and sky.
-We stand on the edge
-of the cliff every day.
-I hang onto the plum’s rough trunk,
-stare at the view...memorizing it with you.
-'''
-
-catpoem = '''
-CAT AND I
-
-My cat and I
-sat on the stairs
-leading to the front porch
-last night
-in a golden arc of light.
-A cricket chirped in the thicket
-of English ivy and light blue-green juniper
-tangled around the bright white railing.
-
-My entranced, long-haired cat,
-purred as he sat on a steep step.
-With the rhythmic throbs 
-of the cricket’s sharp chirps
-and my cat’s soothing purrs,
-I was content.
-'''
-
-
-
-#  the dictionary definition needs to be above the functions but below the big strings
-#  search through keys in dictionary
-#  the dictionary must be above the functions to work
- 
-# ==== dictionary ====  
-
-smalldictionary = {
-  "cat": catpoem,
-  "homegardener": homegardenerpoem,
-  "theview": theviewpoem,
+smalldictionary = {           # each poem is a long string stored in module poems
+  "cat": poems.catpoem,       # so each poem has a prefix of poems referencing the module.  
+  "homegardener": poems.homegardenerpoem,
+  "theview": poems.theviewpoem,
+  "december": poems.december,
+  "treebites": poems.treebites,
+  "oldpath": poems.oldpath,
+  "holmesappears": poems.holmesappears,
   }
-  
-  
-# searches entire dictionary for a phrase(substring several words) that references large strings  
+
+
+# searches entire dictionary for a phrase(substring several words) that references large strings
 #####================================================
-#           search for phrase          
+#           search for phrase
 #####================================================
 def search_for_phrase(dname,phrase):
     for key in dname:                  #loops thru dictionary
-        if phrase in dname.get(key): 
-            print("winner " + "in " + key) 
+        if phrase in dname.get(key):
+            print("winner " + "in " + key)
         else:
-            print(key +" no joy")        
-   
+            print(key +" no joy")
+
 
 dname = smalldictionary            #dictionary name
-a_phrase = "ivy and light"         #phrase looking for
+a_phrase = "through the sky"         #phrase looking for
 
 #calling function here
-search_for_phrase(smalldictionary,a_phrase)         
+search_for_phrase(smalldictionary,a_phrase)
 
 
 
@@ -142,7 +70,7 @@ search_for_phrase(smalldictionary,a_phrase)
 
 # count number of times a word is used in a poem(big string)
 # inputs are the poem(big string), and substring
-# using a chain the methods lower() and count() are called 
+# using a chain the methods lower() and count() are called
 # this searches fora substring within a large string
 # This function is designed to count in one string at a time
 # and could easily be incorporated into a loop to be used in the dictionary
@@ -158,7 +86,4 @@ def count_word_match(poem,substring):
 
 
 #example of method call which prints and returns number of occurences
-count_word_match("theviewpoem","tripping")
-
-
-
+count_word_match("poems.theviewpoem","tripping")
